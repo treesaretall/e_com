721bfe6c618fa0c569945c801d35a5f6361703f3
treesaretall <himatthewhurst@hotmail.com>
2023-07-17 15:15:04 -0400

    made server hit of products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,12 +1,19 @@
 const router = require('express').Router();
+const { Model } = require('sequelize');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }]
+    });
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ err: err });
+  };
 });
 
 // get one product
